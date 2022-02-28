bce616829fee5086e15cf4284b93550b5cc92a8f
Valery Boltavsky <valeryboltavsky@valerybavskymbp.dm.cx>
2022-02-28 16:02:55 +0200

    Updates

---
+++ precache-manifest.f09750ffc489068408419bb32d93beb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dd053286126e98e10ce",
+    "url": "/robofriends/static/css/main.74399efa.chunk.css"
+  },
+  {
+    "revision": "0dd053286126e98e10ce",
+    "url": "/robofriends/static/js/main.0dd05328.chunk.js"
+  },
+  {
+    "revision": "055cc255bfa7fd24a8d3",
+    "url": "/robofriends/static/css/1.d658a8c8.chunk.css"
+  },
+  {
+    "revision": "055cc255bfa7fd24a8d3",
+    "url": "/robofriends/static/js/1.055cc255.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "f8d6a76eed3a7bf6dec1fa7f13fe12df",
+    "url": "/robofriends/index.html"
+  }
+];
